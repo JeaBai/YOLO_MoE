3ad11799fe5c07822e149b91bfecd6967177b8b9
UltralyticsAssistant <web@ultralytics.com>
2026-09-23 04:11:53 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 from pathlib import Path
 
 import torch
-import torch.nn as nn
+from torch import nn
 
 from ultralytics.nn.autobackend import check_class_names
 from ultralytics.nn.modules import (
@@ -26,16 +26,22 @@ from ultralytics.nn.modules import (
     SPPELAN,
     SPPF,
     A2C2f,
+    A3C2f,
     AConv,
     ADown,
     Bottleneck,
     BottleneckCSP,
     C2f,
+    C2f_LSKA,
     C2fAttn,
     C2fCIB,
     C2fPSA,
     C3Ghost,
     C3k2,
+    C3k2_Dynamic,
+    C3k2MA,
+    C3k2MA_Lite,
+    C3k2UltraPro,
     C3x,
     CBFuse,
     CBLinear,
@@ -47,6 +53,7 @@ from ultralytics.nn.modules import (
     Detect,
     DWConv,
     DWConvTranspose2d,
+    DyC2f,
     Focus,
     GhostBottleneck,
     GhostConv,
@@ -66,21 +73,14 @@ from ultralytics.nn.modules import (
     SCDown,
     Segment,
     Segment26,
+    SparseDualMoE,
     TorchVision,
+    WaveC2f,
     WorldDetect,
     YOLOEDetect,
     YOLOESegment,
     YOLOESegment26,
     v10Detect,
-    C3k2_Dynamic,
-    C2f_LSKA,
-    WaveC2f,
-    DyC2f,
-    A3C2f,
-    C3k2UltraPro,
-    C3k2MA,
-    C3k2MA_Lite,
-    SparseDualMoE
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, LOGGER, WINDOWS, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -1386,11 +1386,9 @@ class SafeClass:
 
     def __init__(self, *args, **kwargs):
         """Initialize SafeClass instance, ignoring all arguments."""
-        pass
 
     def __call__(self, *args, **kwargs):
         """Run SafeClass instance, ignoring all arguments."""
-        pass
 
 
 class SafeUnpickler(pickle.Unpickler):
@@ -1625,7 +1623,7 @@ def parse_model(d, ch, verbose=True):
             C3k2UltraPro,
             C3k2MA,
             C3k2MA_Lite,
-            SparseDualMoE
+            SparseDualMoE,
         }
     )
     repeat_modules = frozenset(  # modules with 'repeat' arguments
